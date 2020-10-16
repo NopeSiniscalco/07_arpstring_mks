92926de41600830e470776053e33c0410c894454
fsiniscalco10@stuy.edu <fsiniscalco10@stuy.edu>
2020-10-16 15:06:43 +0200

    updated arpstring.c

--- arpstring.c
+++ arpstring.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 
-void print_array(int *a, int length){
+void print_array(int *a, int length)
+{
 	int i;
 	printf("Array: [");
-	for(i=0;i<length;i++)
-		printf("%d ",a[i]);
+	for (i = 0; i < length; i++)
+		printf("%d ", a[i]);
 	printf("] \n");
 }
 
@@ -25,10 +26,23 @@ float array_avg(int *p, int length)
 int array_copy(int *a, int *b, int length)
 {
 	int i;
-	for(i=0;i<length;i++){
-		*(b+i)=a[i];
+	for (i = 0; i < length; i++)
+	{
+		*(b + i) = a[i];
 	}
 	return 0;
+}
+
+int string_length(char *a)
+{
+	int length = 0;
+	int i = 0;
+	while (a[i])
+	{
+		length++;
+		i++;
+	}
+	return length;
 }
 
 int main()
@@ -39,14 +53,17 @@ int main()
 	{
 		a[i] = i;
 	}
-	print_array(a,10);
+	print_array(a, 10);
 	printf("array avg: %f\n", array_avg(a, 10));
 
 	int b[10];
-	print_array(a,10);
-	print_array(b,10);
+	print_array(a, 10);
+	print_array(b, 10);
 	printf("copied array: \n");
-	array_copy(a,b,10);
-	print_array(b,10);
+	array_copy(a, b, 10);
+	print_array(b, 10);
+
+	char *c = "StringTest";
+	printf("Length of string: %d\n", string_length(c));
 	return 0;
 }
